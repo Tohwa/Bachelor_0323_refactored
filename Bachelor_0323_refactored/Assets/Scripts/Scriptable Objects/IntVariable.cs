4ee02ccb0d0458cf058eca9af72ba9d4777366c5
Tohwa | Sascha <Tohwa.gaming@gmail.com>
2023-11-08 14:11:43 +0100

    advanced folder structure and preemptivly created scritps for tommorows worktime

---
+++ Bachelor_0323_refactored/Assets/Scripts/Scriptable Objects/IntVariable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newIntegerVariable", menuName = "Variable/Integer")]
+public class IntVariable : ScriptableObject
+{
+    public int Value;
+}
